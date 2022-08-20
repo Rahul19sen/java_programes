c8905e151e207edef18260e81201a31731f4f291
Rahul19sen <110010996+Rahul19sen@users.noreply.github.com>
2022-08-20 15:43:11 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+// program hello world 
+// programed by Rahul sen
+import java.util.*
+class Hello{
+public static void main(String []args){
+System.out.println("Hello world");
+}
+}
+
+//Hello world
